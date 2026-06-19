31e79ed172d7fa705db2443deac66f1219dd3d97
UtkarshCoder007 <utkarsh.prgmr@gmail.com>
2026-06-19 14:34:27 +0530

    chore: initialize Spring Boot project scaffold

---
+++ src/main/java/com/luminate/luminate/LuminateApplication.java
@@ -0,0 +1,13 @@
+package com.luminate.luminate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuminateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuminateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luminate/luminate/LuminateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luminate.luminate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuminateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
