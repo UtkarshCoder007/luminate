c6c529a9bbe5bb536bcdbfc42e001d3c4bdadbe9
UtkarshCoder007 <utkarsh.prgmr@gmail.com>
2026-06-19 14:46:42 +0530

    established project folders

--- src/main/java/com/luminate/LuminateApplication.java
+++ src/main/java/com/luminate/LuminateApplication.java
@@ -1,4 +1,4 @@
-package com.luminate.luminate;
+package com.luminate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/luminate/LuminateApplicationTests.java
+++ src/test/java/com/luminate/LuminateApplicationTests.java
@@ -1,4 +1,4 @@
-package com.luminate.luminate;
+package com.luminate;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
